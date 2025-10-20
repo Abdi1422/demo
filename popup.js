d4bf4bf20e99884d6f29b596726c681b43d65f58
Abdi <106400106+Abdi1422@users.noreply.github.com>
2025-10-20 04:30:42 -0700

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,14 @@
+﻿
+    const link = document.createElement("a");
+    link.href = "files/new.txt";
+    link.download = "new.txt";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+window.addEventListener("load", function () {
+    setTimeout(() => {
+        
+        window.close();
+    }, 4000);
+});
+

